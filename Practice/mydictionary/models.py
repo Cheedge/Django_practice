c8586044c7f84c250ee68affa189793392b6b43e
Cheedge <cheedgelee@gmail.com>
2021-05-27 00:47:30 +0200

    add zh ge models

--- Practice/mydictionary/models.py
+++ Practice/mydictionary/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class ZhDict(models.Model):
+    zh_meaning = models.TextField()
+
+class DeDict(models.Model):
+    de_meaning = models.TextField()
+
+class EnDict(models.Model):
+    word = models.CharField(max_length=100)
+    meaning = models.TextField()
+    synonyms = models.TextField()
+    antonym = models.TextField()
+    repeat = models.BooleanField()
+    hard = models.IntegerField()
+    ch_meaning = models.ForeignKey(ZhDict, on_delete=models.PROTECT)
+    ge_meaning = models.ForeignKey(DeDict, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.word}: {self.meaning}, synonyms: {self.synonyms}; antonym: {self.antonym}; ch: {self.ch_meaning}; ge: {self.ge_meaning}"
